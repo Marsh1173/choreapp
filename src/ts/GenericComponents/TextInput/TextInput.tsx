959ecfef86bc741583ead21e4ba5e50672b4b786
Nate Roylance <natehroylance@gmail.com>
2021-11-14 21:31:33 -0700

    Input test X

---
+++ src/ts/GenericComponents/TextInput/TextInput.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ChangeEvent } from "react";
+
+interface TextInputState {
+    inputValue?: string;
+}
+
+export class TextInput extends React.Component<{}, TextInputState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            inputValue: "",
+        };
+    }
+
+    render() {
+        return <input className="TextInput" value={this.state.inputValue} onChange={(e) => this.updateInputValue(e)} />;
+    }
+
+    updateInputValue(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            inputValue: e.target.value,
+        });
+    }
+
+    getValue(): string {
+        if (this.state.inputValue) {
+            return this.state.inputValue;
+        } else {
+            return "";
+        }
+    }
+}
--- src/ts/Screens/AddTaskScreen/AddTaskScreen.tsx
+++ src/ts/Screens/AddTaskScreen/AddTaskScreen.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { TextInput } from "../../GenericComponents/TextInput/TextInput";
 import "./AddTaskScreenStyles.less";
 
 export const AddTaskScreen: React.FC<AddTaskProps> = (props) => {
+    let nameInputRef: React.RefObject<TextInput> = React.createRef();
+
     const onCreate = () => {
+        if (nameInputRef.current) {
+            console.log(nameInputRef.current.getValue());
+        }
         props.closeScreen();
     };
 
     return (
         <div className="AddTaskScreen">
-            <input className="nameInput" type="text" />
+            <TextInput ref={nameInputRef}></TextInput>
             <div className="buttonsDiv">
                 <button className="cancelButton clickScaleBig" onMouseUp={() => props.closeScreen()}>
                     Cancel
